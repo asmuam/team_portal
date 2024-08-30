0e586e9d4dd2c607868e5108b2229e73d771190b
ADIB SULTHON MUAMMAL <222111840@stis.ac.id>
2024-08-30 12:33:36 +0700

    ada pegawai, ada admin

--- backend/prisma/dummy.sql
+++ backend/prisma/dummy.sql
@@ -1,6 +1,6 @@
 INSERT INTO `user` (`id`, `username`, `password`, `role`,`name`) VALUES
-	(1, 'adib', '$2a$12$d8xzHE5UOU8QgM4BE5G.YeOGDyXyu7mDeYxOvt620CQkuRHC4iugK', 'admin', 'Adib Sulthon Asmuamal'),
-	(2, 'hima', '$2a$12$d8xzHE5UOU8QgM4BE5G.YeOGDyXyu7mDeYxOvt620CQkuRHC4iugK', 'admin','Himawan Wahid Ikhwansyah'),
+	(1, 'adib', '$2a$12$d8xzHE5UOU8QgM4BE5G.YeOGDyXyu7mDeYxOvt620CQkuRHC4iugK', 'pegawai', 'Adib Sulthon Asmuamal'),
+	(2, 'hima', '$2a$12$d8xzHE5UOU8QgM4BE5G.YeOGDyXyu7mDeYxOvt620CQkuRHC4iugK', 'pegawai','Himawan Wahid Ikhwansyah'),
 	(3, 'lilis', '$2a$12$d8xzHE5UOU8QgM4BE5G.YeOGDyXyu7mDeYxOvt620CQkuRHC4iugK', 'admin','Lilis Dwiyanti'),
 	(4, 'hadi', '$2a$12$d8xzHE5UOU8QgM4BE5G.YeOGDyXyu7mDeYxOvt620CQkuRHC4iugK', 'admin','Setya Hadi Nugroho');
 
