8e1c44bf9ec6d7d92d24a1b077bfd45cd26e8a4c
ADIB SULTHON MUAMMAL <222111840@stis.ac.id>
2024-09-03 17:41:22 +0700

    middleware auth token dan role

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,24 +1,32 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-// Ambil JWT_SECRET dari environment variables
-const JWT_SECRET = process.env.JWT_SECRET;
+dotenv.config();
 
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+// Middleware untuk memeriksa dan memverifikasi token
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  if (!authHeader) {
-    return res.status(401).json({
-      message: "Unauthorized",
-    });
-  }
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1]; // Mengambil token dari header Authorization
 
-  const token = authHeader.split(' ')[1];
-  if (token == null) return res.sendStatus(401);
+  if (token == null) return res.sendStatus(401); // Jika token tidak ada, kirim status 401 (Unauthorized)
 
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403); // Jika token tidak valid, kirim status 403 (Forbidden)
+    req.user = user; // Simpan informasi pengguna dalam objek request
+    next(); // Lanjutkan ke rute berikutnya
   });
 };
 
-export default authenticateToken;
+// Middleware untuk memeriksa peran pengguna
+const authorizeRole = (roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Akses ditolak" }); // Jika peran tidak diizinkan, kirim status 403 (Forbidden)
+    }
+    next(); // Lanjutkan ke rute berikutnya
+  };
+};
+
+export { authenticateToken, authorizeRole };
